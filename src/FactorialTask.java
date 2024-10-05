90c1ed1ceade52d42639b5e008821b8f6b9c2a49
andreyJdev <andrey.jdev@gmail.com>
2024-10-05 16:26:26 +0300

    ForkJoinPoolTask

---
+++ src/FactorialTask.java
@@ -0,0 +1,21 @@
+import java.util.concurrent.RecursiveTask;
+
+class FactorialTask extends RecursiveTask<Long> {
+    private final int n;
+
+    public FactorialTask(int n) {
+        this.n = n;
+    }
+
+    @Override
+    protected Long compute() {
+        if (n <= 1) {
+            return 1L;
+        }
+
+        FactorialTask subTask = new FactorialTask(n - 1);
+        subTask.fork();
+
+        return n * subTask.join();
+    }
+}
