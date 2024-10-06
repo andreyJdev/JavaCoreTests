d469eb782586eab3a3dc1a9aa3a8ffc00d23d179
andreyJdev <andrey.jdev@gmail.com>
2024-10-06 15:40:27 +0300

    MavenUpdLibTask

--- GeometryApp/src/main/java/ru/project/geometry/App.java
+++ GeometryApp/src/main/java/ru/project/geometry/App.java
@@ -9,7 +9,7 @@ public class App
     public static void main(String[] args) {
         Circle circle = new Circle(10);
         Rectangle rectangle = new Rectangle(3, 6);
-        Triangle triangle = new Triangle(2, 3, 5);
+        Triangle triangle = new Triangle(4, 4, 4);
 
         System.out.println("Circle area: " + circle.getArea());
         System.out.println("Circle perimeter: " + circle.getPerimeter());
